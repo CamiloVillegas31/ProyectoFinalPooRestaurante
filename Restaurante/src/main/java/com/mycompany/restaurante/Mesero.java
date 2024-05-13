5b005d8ef17e0971b640abb5165fb6dd2798120e
Camilo Andres Villegas Flor <162384786+CamiloVillegas31@users.noreply.github.com>
2024-05-13 11:44:44 -0500

    sfas
    ok

---
+++ Restaurante/src/main/java/com/mycompany/restaurante/Mesero.java
@@ -0,0 +1,13 @@
+/* Proyecto final Poo Ingeniería de sistemas nocturno    
+ * Integrantes: Jose Luis Campo Zuñiga y Camilo Andrés Villegas Flor  
+ */
+
+package com.mycompany.restaurante;
+
+
+public class Mesero {
+    
+    public String nameWaiter;        //Nombre del mesero
+    public String IdWaiter;          //Identificador Mesero
+    
+}
---
+++ Restaurante/src/main/java/com/mycompany/restaurante/Restaurante.java
@@ -0,0 +1,12 @@
+/* Proyecto final Poo Ingeniería de sistemas nocturno    
+ * Integrantes: Jose Luis Campo Zuñiga y Camilo Andrés Villegas Flor  
+ */
+
+package com.mycompany.restaurante;
+
+public class Restaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
